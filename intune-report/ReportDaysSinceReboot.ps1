ae01cb69305242a86859e8e25872d51a5c3e9870
Andrew Welch <awelch@law.virginia.edu>
2024-02-20 13:39:46 -0500

    first commit

---
+++ intune-report/ReportDaysSinceReboot.ps1
@@ -0,0 +1,12 @@
+﻿# Author: Andrew Welch (aw5qq@virginia.edu)
+# Description: This script reports the number of days since the last reboot
+
+# Check if the computer has been rebooted in the last 30 days
+$Uptime= get-computerinfo | Select-Object OSUptime 
+if ($Uptime.OsUptime.Days -ge 30){
+    Write-Output "$($Uptime.OsUptime.Days)"
+    Exit 1
+}else {
+    Write-Output "$($Uptime.OsUptime.Days)"
+    Exit 0
+}
