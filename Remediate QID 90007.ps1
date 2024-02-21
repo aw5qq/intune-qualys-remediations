1f41b28878250a3950848a3eda9c0b1c225a067e
Andrew Welch <awelch@law.virginia.edu>
2024-02-21 14:06:25 -0500

    added registry key check

--- Remediate QID 90007.ps1
+++ Remediate QID 90007.ps1
@@ -1,6 +1,23 @@
-﻿# Author: Andrew Welch (aw5qq@lawschool.virginia.edu)
-# This script will set the CachedLogonsCount registry key to 0. 
-# This will prevent cached credentials from being stored on the system. 
-# This is a remediation script for QID 90007.
+﻿# Author: Andrew Welch (aw5qq@virginia.edu)
+# Description: This script sets the CachedLogonsCount registry key to 0
+# QID: 90007
 
-Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "CachedLogonsCount" -Value 0
+# Define the registry key
+$Path = "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon"
+$Name = "CachedLogonsCount"
+$Value = 0
+
+# Check if the registry key exists and if it does, set the value to 0
+Try {
+    if (Test-Path -Path $Path) {
+        Set-ItemProperty -Path $Path -Name $Name -Value $Value -ErrorAction Stop
+        Write-Output "CachedLogonsCount has been set to 0"
+        Exit 0
+    }
+    Write-Output "Unable to set CachedLogonsCount to 0"
+    Exit 1
+} 
+Catch {
+    Write-Output "Unable to set CachedLogonsCount to 0"
+    Exit 1
+}
