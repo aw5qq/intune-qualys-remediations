39be0cab2c0400b30d153bfd52c4f680599f5336
Andrew Welch <awelch@law.virginia.edu>
2024-02-21 15:03:53 -0500

    fixed header & comments

--- Remediate QID 105576.ps1
+++ Remediate QID 105576.ps1
@@ -1,10 +1,10 @@
-﻿# Remediate QID 105576
-# https://cve.report/qid/105576
-
+﻿# Author: Andrew Welch (aw5qq@virginia.edu)
+# Description: This script removes XML Parser (MSXML) 4.0
+# QID: 105576
 
 $msxml4Products = Get-WmiObject -Query "SELECT * FROM Win32_Product WHERE Name LIKE 'Microsoft XML Parser%' OR Name LIKE 'Microsoft XML Core Services 4.0%'"
 
-if ($msxml4Products -eq $null) {
+if ($null -eq $msxml4Products) {
     Write-Host "No Microsoft XML Parser or Microsoft XML Core Services (MSXML) 4.0 installations found."
 
     # Define the path of the msxml4.dll file
@@ -33,6 +33,3 @@ foreach ($product in $msxml4Products) {
         Write-Host "Failed to uninstall $($product.Name). Error code: $($result.ReturnValue)"
     }
 }
-
-
-
--- Remediate QID 105943.ps1
+++ Remediate QID 105943.ps1
@@ -1,11 +1,12 @@
-﻿# Remediate QID 105943
-
+﻿# Author: Andrew Welch (aw5qq@virginia.edu)
+# Description: This script removes Adobe Flash Player
+# QID: 105943
 
 # Check if Flash Player is installed
 $flashPlayer = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -like "Adobe Flash" }
 
+# Uninstall Flash Player if it is installed
 if ($flashPlayer) {
-    # Uninstall Flash Player
     $uninstallResult = Start-Process -FilePath "msiexec.exe" -ArgumentList "/x $($flashPlayer.IdentifyingNumber) /qn" -Wait -PassThru
     if ($uninstallResult.ExitCode -eq 0) {
         Write-Host "Uninstalled $($flashPlayer.Name) successfully."
