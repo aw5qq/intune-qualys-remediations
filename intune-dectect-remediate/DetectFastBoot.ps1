1d86c35855c7c84fc1f64aed51512232c32f810b
Andrew Welch <awelch@law.virginia.edu>
2024-02-21 11:51:12 -0500

    first commit

---
+++ intune-dectect-remediate/DetectFastBoot.ps1
@@ -0,0 +1,24 @@
+﻿# Author: Andrew Welch (aw5qq@virginia.edu)
+# Description: This script detects if Fast Boot is enabled
+
+# Define the registry key
+$Path = "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power"
+$Name = "HiberbootEnabled"
+$Value = 0
+
+# Check if the registry key exists and if it does, check if the value is correct
+Try {
+    if (Test-Path -Path $Path) {
+        $Registry = Get-ItemProperty -Path $Path -Name $Name -ErrorAction Stop | Select-Object -ExpandProperty $Name
+        If ($Registry -eq $Value){
+            Write-Output "Fast Boot is not enabled"
+            Exit 0
+        } 
+    }
+    Write-Output "Fast Boot is enabled"
+    Exit 1
+} 
+Catch {
+    Write-Output "Unable to determine if Fast Boot is enabled"
+    Exit 1
+}
---
+++ intune-dectect-remediate/RemediateFastBoot.ps1
@@ -0,0 +1,25 @@
+﻿# Author: Andrew Welch (aw5qq@virginia.edu)
+# Description: This script disables Fast Boot
+
+# Define the registry key
+$Path = "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power"
+$Name = "HiberbootEnabled"
+$Value = 0
+
+# Check if the registry key already exists
+if (Test-Path -Path $Path) {
+    # Set the registry key
+    try {
+        Set-ItemProperty -Path $Path -Name $Name -Value $Value -ErrorAction Stop
+        Write-Output "Fast Boot has been disabled"
+        Exit 0
+    }
+    catch {
+        Write-Output "Failed to set the registry key: $_"
+        Exit 1
+    }
+}
+else {
+    Write-Output "Registry key does not exist"
+    Exit 1
+}
