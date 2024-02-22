ae21fc5ef6828baac42d1ed463dca2450e2badcb
Andrew Welch <awelch@law.virginia.edu>
2024-02-22 08:13:26 -0500

    first commit

---
+++ QualysAgentTrigger.ps1
@@ -0,0 +1,14 @@
+﻿# Author: Andrew Welch (aw5qq@lawschool.virginia.edu)
+# This script is designed to trigger a Qualys user agent to perform a scan.
+
+# Set the ScanOnDemand variable to 1 to trigger a scan.
+$ScanOnDemand = 1
+
+$paths = @(
+    "HKLM:\SOFTWARE\Qualys\QualysAgent\ScanOnDemand\Inventory",
+    "HKLM:\SOFTWARE\Qualys\QualysAgent\ScanOnDemand\Vulnerability"
+)
+
+foreach ($path in $paths) {
+    Set-ItemProperty –Path $path -Name "ScanOnDemand" -Value $ScanOnDemand
+}
