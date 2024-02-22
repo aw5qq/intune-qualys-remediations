fcf8a499ef5af5c245d06fce2b480bf5fd8c9742
Andrew Welch <awelch@law.virginia.edu>
2024-02-22 08:10:19 -0500

    first commit

---
+++ winget/WingetAll.ps1
@@ -0,0 +1,20 @@
+﻿# Author: Andrew Welch (aw5qq@virginia.edu))
+# Description: This script will upgrade all apps installed via winget
+
+$wingetexe = (Get-Command winget.exe).Source
+
+# Check if the alias already exists
+if (Get-Alias sysget -ErrorAction SilentlyContinue) {
+  # Remove the alias if it already exists
+  Remove-Item alias:sysget
+}
+
+# Create the sysget alias so winget can be ran as system
+New-Alias -Name sysget -Value "$wingetexe" -ErrorAction SilentlyContinue
+
+# Upgrade all apps
+try {
+    sysget upgrade --accept-package-agreements --all --include-unknown --accept-source-agreements --locale en-US
+} catch {
+    Write-Host "An unexpected error occurred while executing the command: $_"
+}
