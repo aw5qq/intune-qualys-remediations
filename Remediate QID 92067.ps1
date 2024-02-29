e5bab7b346ac604c75d4b8885fa9e2a83d16e77e
Andrew Welch <awelch@law.virginia.edu>
2024-02-29 09:09:09 -0500

    first commit

---
+++ Remediate QID 92067.ps1
@@ -0,0 +1,25 @@
+# Author: Andrew Welch (aw5qq@virginia.edu)
+# Description: This script remediates the "EnableCertPaddingCheck" registry key to prevent the "CertPaddingCheck" vulnerability.
+# QID: 92067
+
+# Define the registry key paths and values to add
+$Key1Path = "HKLM:\SYSTEM\CurrentControlSet\Services\HTTP\Parameters"
+$Key1ValueName = "EnableHttp2Tls"
+$Key1ValueData = "1"
+
+try {
+    # Create the registry key path for Key1 if it doesn't exist
+    if (!(Test-Path $Key1Path)) {
+        New-Item -Path $Key1Path -Force
+        Write-Output "$Key1Path Created"
+    }
+    
+    # Create the registry key and value for Key1
+    Set-ItemProperty -Path $Key1Path -Name $Key1ValueName -Value $Key1ValueData
+    Write-Output "$Key1ValueName added to $Key1Path"
+
+    Write-Output "Registry updated successfully."
+}
+catch {
+    Write-Output "Error occurred while updating the registry: $_"
+}
