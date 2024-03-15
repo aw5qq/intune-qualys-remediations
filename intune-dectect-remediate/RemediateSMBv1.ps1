046dd7e79a7b8f0bbe517e5db000595fd292e67b
Andrew Welch <awelch@law.virginia.edu>
2024-03-15 07:51:10 -0400

    .

--- intune-dectect-remediate/RemediateSMBv1.ps1
+++ intune-dectect-remediate/RemediateSMBv1.ps1
@@ -1,7 +1,10 @@
+# Author: Andrew Welch (aw5qq@virginia.edu)
+# Description: This script detects SMBv1 protocol
+
 $SMBv1 = Get-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
 
 if ($SMBv1 -and $SMBv1.State -eq "Enabled") {
-    Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol -NoRestart
+    Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
     Write-Host "SMBv1 protocol has been disabled."
     Exit
 }
@@ -9,7 +12,11 @@ elseif ($SMBv1 -and $SMBv1.State -eq "Disabled") {
     Write-Host "SMBv1 protocol is already disabled."
     Exit
 }
-else {
+elseif ($null -eq $SMBv1) {
     Write-Host "SMBv1 protocol is not found."
+    Exit
+}
+else {
+    Write-Host "An error occurred while retrieving SMBv1 protocol information."
     Exit
 }
