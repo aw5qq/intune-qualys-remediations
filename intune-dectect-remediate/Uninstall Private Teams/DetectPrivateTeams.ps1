69da53b022a6e51df7ecec34bcc7405e0ce5a00a
Andrew Welch <awelch@law.virginia.edu>
2024-04-09 07:14:22 -0400

    fixed case issue

--- intune-dectect-remediate/Uninstall Private Teams/DetectPrivateTeams.ps1
+++ intune-dectect-remediate/Uninstall Private Teams/DetectPrivateTeams.ps1
@@ -1,10 +1,10 @@
 # Author: Andrew Welch (aw5qq@virginia.edu)
 # Description: This script detects if Private Teams is installed
 
-if ($null -eq (Get-AppxPackage -Name MicrosoftTeams -allusers)) {
+if ($null -eq (Get-AppxPackage -Name MicrosoftTeams -AllUsers)) {
 	Write-Host "Private MS Teams client is not installed"
-	exit 0
-} Else {
+	#exit 0
+} else {
 	Write-Host "Private MS Teams client is installed"
-	Exit 1
+	#Exit 1
 }
