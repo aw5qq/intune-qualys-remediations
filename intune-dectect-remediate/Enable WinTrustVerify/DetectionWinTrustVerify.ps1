fe37a5238a6e0b73663df27d622c9cd7b46aa9ed
Andrew Welch <awelch@law.virginia.edu>
2024-04-18 08:00:24 -0400

    .first commit

---
+++ intune-dectect-remediate/Enable WinTrustVerify/DetectionWinTrustVerify.ps1
@@ -0,0 +1,46 @@
+# Author: Andrew Welch (aw5qq@virginia.edu)
+# Description: This script checks if the WinTrustVerify setting is enabled 
+
+$registryKey1 = "HKLM:\SOFTWARE\Microsoft\Cryptography\Wintrust\Config"
+$registryKey2 = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Cryptography\Wintrust\Config"
+
+$value1 = "EnableCertPaddingCheck"
+$expectedData1 = "1"
+
+$value2 = "EnableCertPaddingCheck"
+$expectedData2 = "1"
+
+$issues = @()
+
+# Function to check a registry value
+function CheckValues {
+    param (
+        [string]$Path,
+        [string]$Name,
+        [string]$ExpectedData
+    )
+    
+    if (Test-Path $Path) {
+        $data = (Get-ItemProperty -Path $Path).$Name
+        if ($null -eq $data) {
+            $issues += "Value '$Name' does not exist in '$Path'"
+        } elseif ($data -ne $ExpectedData) {
+            $issues += "Value '$Name' in '$Path' does not match expected data. Found: '$data', Expected: '$ExpectedData'"
+        }
+    } else {
+        $issues += "Registry Key does not exist: '$Path'"
+    }
+}
+
+# Check the registry values
+CheckValues -Path $registryKey1 -Name $value1 -ExpectedData $expectedData1
+CheckValues -Path $registryKey2 -Name $value2 -ExpectedData $expectedData2
+
+# Output the results
+if ($issues.Count -gt 0) {
+    Write-Output "Issues found: $($issues -join ', ')"
+    Exit 0
+} else {
+    Write-Output "All checks passed."
+    Exit 1
+}
