4141d981c34af1dc2c41ed7816479cb1450d7555
Andrew Welch <awelch@law.virginia.edu>
2024-09-18 12:57:43 -0400

    .

--- intune-dectect-remediate/DaysSinceReboot/DetectDaysSinceReboot.ps1
+++ intune-dectect-remediate/DaysSinceReboot/DetectDaysSinceReboot.ps1
@@ -3,7 +3,7 @@
 
 # Check if the computer has been rebooted in the last 30 days
 $Uptime= get-computerinfo | Select-Object OSUptime 
-if ($Uptime.OsUptime.Days -ge 30){
+if ($Uptime.OsUptime.Days -ge 14){
     Write-Output "$($Uptime.OsUptime.Days)"
     Exit 1
 }else {
